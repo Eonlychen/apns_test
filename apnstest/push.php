cf668c382e2033ee4203629c50d5b23d4df168e1
eonly_chen@163.com <eonly_chen@163.com>
2018-07-20 10:31:10 +0800

    apns test

---
+++ apnstest/push.php
@@ -0,0 +1,48 @@
+<?php
+//这里填写设备的Token码
+$deviceToken = 'a0ef3de5483718e02ac0863ade0b50753c7c70bbbac1892da2be6bf31ffc9984';
+//这里是密钥密码
+$passphrase = '123456';
+//推送的消息
+$message = '恶心的推送服务端';
+
+////////////////////////////////////////////////////////////////////////////////
+
+$ctx = stream_context_create();
+stream_context_set_option($ctx, 'ssl', 'local_cert', 'ck.pem');//ck文件
+stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+
+// Open a connection to the APNS server
+$fp = stream_socket_client(
+    'ssl://gateway.sandbox.push.apple.com:2195', $err,
+    $errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+
+if (!$fp)
+    exit("Failed to connect: $err $errstr" . PHP_EOL);
+
+echo 'Connected to APNS' . PHP_EOL;
+
+// Create the payload body
+$body['aps'] = array(
+    'alert' => $message,
+    'sound' => 'default'
+    );
+
+// Encode the payload as JSON
+$payload = json_encode($body);
+
+// Build the binary notification
+$msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
+
+// Send it to the server
+$result = fwrite($fp, $msg, strlen($msg));
+
+if (!$result)
+    echo 'Message not delivered' . PHP_EOL;
+else
+    echo 'Message successfully delivered' . PHP_EOL;
+
+// Close the connection to the server
+fclose($fp);
+    
+?>
